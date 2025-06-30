0764571e16924f6c1a81005d5294ba2eecb3e2a1
rima1881 <amd.gharibipour@outlook.com>
2025-06-30 09:58:04 -0400

    extracted product name

---
+++ readme.md
@@ -0,0 +1,17 @@
+
+product selector:
+```
+    class="css-0"
+```
+
+
+product name:
+```
+    data-testid='product-title'
+```
+
+
+product price
+```
+    data-testid='price-product-tile'
+```
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,8 +16,14 @@ fun main() {
         playwright.selectors().setTestIdAttribute("data-testid")
 
         page.waitForSelector("[data-testid='product-image']", Page.WaitForSelectorOptions())
-        val html = page.content()
-        println(html)
+//        val html = page.content()
+//        println(html)
+
+        val items = page.querySelectorAll("[data-testid='product-title']")
+
+        for (item in items) {
+            println(item.innerText())
+        }
 
         browser.close()
     }
