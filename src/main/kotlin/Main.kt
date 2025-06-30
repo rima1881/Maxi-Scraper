f7c6122928056c1ce473a5195ada8a6d21e73f8f
rima1881 <amd.gharibipour@outlook.com>
2025-06-30 09:37:33 -0400

    added loading page

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 package org.example
 import com.microsoft.playwright.*
+import com.microsoft.playwright.options.LoadState
+import com.microsoft.playwright.options.WaitForSelectorState
+import com.microsoft.playwright.options.WaitUntilState
 
 
 fun main() {
@@ -8,8 +11,11 @@ fun main() {
             BrowserType.LaunchOptions().setHeadless(true)
         )
         val page = browser.newPage()
-        page.navigate("https://google.com")
 
+        page.navigate("https://www.maxi.ca/en/search?search-bar=eggs")
+        playwright.selectors().setTestIdAttribute("data-testid")
+
+        page.waitForSelector("[data-testid='product-image']", Page.WaitForSelectorOptions())
         val html = page.content()
         println(html)
 
